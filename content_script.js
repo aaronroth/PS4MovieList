359753cb31bcce0dafd1ac1413554dedc6b0377b
Aaron Roth <aaronroth28@gmail.com>
2017-04-23 18:34:05 -0400

    Fully functional.

--- content_script.js
+++ content_script.js
@@ -1,9 +1,12 @@
 var links = document.getElementsByClassName("detLink");
 
+// Changes the background color of all listings with "264" within the title.
 for (var i = 0; i < links.length; i++) { 
     var title = links[i].innerHTML;
+    var regex = /264/;
+    var result = regex.test(title);
     
-    if (title === "Logan.2017.HC.HDRip.XViD.AC3-ETRG") {
-        links[i].parentNode.parentNode.parentNode.remove();
+    if (result) {
+        links[i].parentNode.parentNode.parentNode.setAttribute("style", "background-color: #99fb98");
     }
 }
